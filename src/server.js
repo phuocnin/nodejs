b07083239eee717bc5e9a399af7b5b83f3154c51
your name <phuocnin26@gmail.com>
2022-08-25 16:14:43 +0700

    env

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import configviewEngine from './configs/viewEngine';
+require('dotenv').config();
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT;
+
 
 configviewEngine(app);
 app.get('/', (req, res) => {
