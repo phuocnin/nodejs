e63eb97631ce2eaa5503779877ce07e33fb3d70b
your name <phuocnin26@gmail.com>
2022-08-25 16:02:46 +0700

    view engine, install docenv

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 import express from 'express';
-const path = require('path');
+import configviewEngine from './configs/viewEngine';
 
 const app = express();
 const port = 8080;
 
+configviewEngine(app);
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './index.html'));
+    res.render('index.ejs')
 })
 
 app.listen(port, () => {
