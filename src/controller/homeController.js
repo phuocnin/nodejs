3d57a80e6c571eddbb10d605def94294d22a46d2
phuocnin <phuocnin26@gmail.com>
2022-11-18 00:05:18 +0700

    upload file

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -112,26 +112,27 @@ let upfile = (req, res) => {
     con.connect(function (x) {
 
 
-        upload(req, res, function (err) {
-            // req.file contains information of uploaded file
-            // req.body contains information of text fields, if there were any
-
-            if (req.fileValidationError) {
-                return res.send(req.fileValidationError);
-            }
-            else if (!req.file) {
-                return res.send('Please select an image to upload');
-            }
-            else if (err instanceof multer.MulterError) {
-                return res.send(err);
-            }
-            else if (err) {
-                return res.send(err);
-            }
-
-            // Display uploaded image for user validation
-            res.send(`You have uploaded this image: <hr/><img src="/image/${req.file.fieldname}" width="500"><hr /><a href="./">Upload another image</a>`);
-        });
+        // upload(req, res, function (err) {
+        //     // req.file contains information of uploaded file
+        //     // req.body contains information of text fields, if there were any
+        //     console.log(err)
+        //     console.log(req.file)
+        //     console.log(req.fileValidationError)
+        if (req.fileValidationError) {
+            return res.send(req.fileValidationError + `<hr href="./upload-file">Upload another image</hr>`);
+        }
+        else if (!req.file) {
+            return res.send('Please select an image to upload' + `<div><a href="./upload-file">Upload another image</a></div>`);
+        }
+        //     else if (err instanceof multer.MulterError) {
+        //         return res.send(err);
+        //     }
+        //     else if (err) {
+        //         return res.send(err);
+        //     }
+        // Display uploaded image for user validation
+        res.send(`You have uploaded this image: <hr/><img src="/image/${req.file.filename}" width="500"><hr /><a href="./">Upload another image</a>`);
+        // });
 
 
     })
